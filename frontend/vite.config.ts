fc12efd74e0ede0c914f68d05fa8f3874be64c8c
AnaJordao <acjpjvjp@gmail.com>
2026-07-23 16:44:52 -0300

    #11 Test config correction

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
       provider: "v8",
       reporter: ["text", "json", "html", "lcov", "json-summary"],
     },
-    exclude: ["**/node_modules/**", "**/dist/**", "backend/**"],
+    include: ["src/**/*.{test,spec}.{ts,tsx}"],
+    exclude: ["**/node_modules/**", "**/dist/**"],
   },
 });
