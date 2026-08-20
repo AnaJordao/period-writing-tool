845b6c38c7d1add630bd861ed1381504fb120610
AnaJordao <acjpjvjp@gmail.com>
2026-08-20 14:37:29 -0300

    #29 Update the ActionGrid component

--- frontend/src/components/ActionGrid/ActionsGrid.tsx
+++ frontend/src/components/ActionGrid/ActionsGrid.tsx
@@ -3,27 +3,35 @@ import classes from './ActionGrid.module.css';
 import type { IconProps } from '@tabler/icons-react';
 import { Fragment } from 'react/jsx-runtime';
 
-interface ActionGridDataProps {
-  data: {
-    sectionName?: string;
-    title: string;
-    icon: React.ForwardRefExoticComponent<IconProps & React.RefAttributes<SVGSVGElement>>;
-    color: string;
-    onClick: () => void | Promise<void>;
-  }[];
+interface ActionGridData {
   title: string;
+  icon: React.ForwardRefExoticComponent<IconProps & React.RefAttributes<SVGSVGElement>>;
+  color: string;
+  onClick: () => void | Promise<void>;
 }
 
-export function ActionGrid({ data, title }: ActionGridDataProps) {
+interface ActionGridSectionProps {
+  sectionName?: string;
+  sectionData: ActionGridData[];
+}
+
+type ActionGridDataProps =
+  | {
+      type: 'flat';
+      data: ActionGridData[];
+      title: string;
+    }
+  | {
+      type: 'sectioned';
+      data: ActionGridSectionProps[];
+      title: string;
+    };
+
+export function ActionGrid({ type, data, title }: ActionGridDataProps) {
   const theme = useMantineTheme();
 
-  const items = data.map((item) => (
-    <Fragment key={item.title}>
-      {item.sectionName && (
-        <Text key={item.sectionName} className={classes.sectionTitle}>
-          {item.sectionName}
-        </Text>
-      )}
+  function renderItem(item: ActionGridData) {
+    return (
       <UnstyledButton
         key={item.title}
         className={classes.item}
@@ -36,17 +44,39 @@ export function ActionGrid({ data, title }: ActionGridDataProps) {
           {item.title}
         </Text>
       </UnstyledButton>
-    </Fragment>
-  ));
+    );
+  }
+
+  const items =
+    type === 'flat'
+      ? data.map((item) => renderItem(item))
+      : data.map((section, index) => (
+          <Fragment key={section.sectionName ?? index}>
+            {section.sectionName && (
+              <Text size="sm" fw={500} mt="md" mb="xs">
+                {section.sectionName}
+              </Text>
+            )}
+
+            <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }} mt="md">
+              {section.sectionData.map((item) => renderItem(item))}
+            </SimpleGrid>
+          </Fragment>
+        ));
 
   return (
     <Card withBorder radius="md" className={classes.card}>
       <Group justify="space-between">
         <Text className={classes.title}>{title}</Text>
       </Group>
-      <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }} mt="md">
-        {items}
-      </SimpleGrid>
+
+      {type === 'flat' ? (
+        <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }} mt="md">
+          {items}
+        </SimpleGrid>
+      ) : (
+        items
+      )}
     </Card>
   );
 }
